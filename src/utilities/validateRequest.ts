bc7858ce597abeadfe2c046dae246bc86f46c3b3
tuanhiep97 <37499012+tuanhiep97@users.noreply.github.com>
2023-07-23 17:26:38 +0700

    Update validateRequest.ts

--- src/utilities/validateRequest.ts
+++ src/utilities/validateRequest.ts
@@ -6,14 +6,14 @@ const imageQueryParamValidate = (
   next: Function,
 ): void => {
   if (request.query.width) {
-    let width = parseInt(request.query.width as string);
+    const width = parseInt(request.query.width as string);
     if (Number.isNaN(width) || width < 1) {
       response.status(400).send('Width param must be a positive number');
     }
   }
 
   if (request.query.height) {
-    let height = parseInt(request.query.height as string);
+    const height = parseInt(request.query.height as string);
     if (Number.isNaN(height) || height < 1) {
       response.status(400).send('Height param must be a positive number');
     }
